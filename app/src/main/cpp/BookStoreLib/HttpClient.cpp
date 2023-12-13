cc7c3c5b2576c3629c42382d347c873a59e4ab90
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2023-12-13 21:29:46 +0000

    feat: AndroidManifest.xml INTERNET permission included + disable ssl server certificate for tests.

--- app/src/main/cpp/BookStoreLib/HttpClient.cpp
+++ app/src/main/cpp/BookStoreLib/HttpClient.cpp
@@ -5,6 +5,7 @@
 
 char* HttpClient::sendHttpGet() {
     httplib::Client cli(googleapisURL);
+    cli.enable_server_certificate_verification(false);
     auto res = cli.Get(path);
     if (res && res->status == 200) {
         const char* body = res->body.c_str();
