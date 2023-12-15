fe7120a72c56536f101ea9939bf89b4f2d8f6e11
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2023-12-15 11:14:37 +0000

    feat: libcurl binary files included

--- app/src/main/cpp/BookStoreLib/include/BookStoreLib/HttpClient.h
+++ app/src/main/cpp/BookStoreLib/include/BookStoreLib/HttpClient.h
@@ -4,12 +4,17 @@
 
 #include <string>
 
-static const char *const googleapisURL = "https://www.googleapis.com";
-static const char *const path = "/books/v1/volumes?q=ios&maxResults=20&startIndex=0";
+
+static const char *const googleapisURL = "http://www.pudim.com.br";
+//static const char *const googleapisURL = "https://www.googleapis.com";
+static const char *const path = "/";
+//static const char *const path = "/books/v1/volumes?q=ios&maxResults=20&startIndex=0";
 
 class HttpClient {
 public:
-    static char* sendHttpGet();
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
+
+    static char *sendHttpGet();
 };
 
 #endif // HTTPCLIENT_H
--- app/src/main/java/com/bookstore/bookstorecsdksibsapplication/MainActivity.java
+++ app/src/main/java/com/bookstore/bookstorecsdksibsapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.bookstore.bookstorecsdksibsapplication;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +14,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,8 +60,49 @@ public class MainActivity extends AppCompatActivity {
      */
     public native String getDataFromJNI();
 
+
+
+    public class FetchDataAsyncTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Call the JNI method in the background thread
+            String jsonDataFromJNI = getDataFromJNI();
+
+            // Perform HTTP GET request to https://www.pudim.com.br
+            try {
+                URL url = new URL("https://www.pudim.com.br");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    InputStream in = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder content = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        content.append(line).append("\n");
+                    }
+
+                    return jsonDataFromJNI + "\n\n" + content;
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return jsonDataFromJNI + "\n\nError fetching data from https://www.pudim.com.br";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("HTTP GET Result", result);
+        }
+    }
+
+
     // AsyncTask to fetch and display books
-    private class FetchDataAsyncTask extends AsyncTask<Void, Void, String> {
+    private class FetchDataAsyncTask_old extends AsyncTask<Void, Void, String> {
 
         @Override
         protected String doInBackground(Void... params) {
